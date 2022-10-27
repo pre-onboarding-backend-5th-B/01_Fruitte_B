451c70d6b46516935239445a4b0ea7990dfb2f0e
codebot053 <codebot053@gmail.com>
2022-10-28 01:14:58 +0900

    feat: cart.models 카트담기 DB구조 변경 기존 Cart 모델은 POST 요청시 product가 아닌 user의 cart가 추가되도록 설계되어 이를 Cart, CartItem 두 모델로 나누어 두 모델이 1:N 관계가 형성될 수 있도록 변경하였습니다.

--- cart/models.py
+++ cart/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from user.models import User
 from product.models import Product
 
-
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class CartItem(models.Model):
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
     is_present = models.BooleanField(default=True)
+
+
+
