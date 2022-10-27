a09c11058993fd8dbecc73f7d3c5e2429bdfffa3
jslee <dlwhdtjd012@kakao.com>
2022-10-27 10:20:41 +0900

    feat(product.views): ProductViewSet init

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
 
 # Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    지원자 view
+    """
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
