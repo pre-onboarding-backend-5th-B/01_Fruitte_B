ca6b2bb97f268bc00c56535603670c5f2c9c0239
jslee <dlwhdtjd012@kakao.com>
2022-10-27 12:07:37 +0900

    feat(product.views): 요청에 따른 Serializer 와 Permission 분기 로직 추가.

--- product/views.py
+++ product/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from product.models import Product
-from product.serializers import ProductSerializer
+from product.serializers import ProductSerializer, ProductListSerializer, \
+    ProductDetailSerializer, ProductWriteSerializer
 
 
 # Create your views here.
@@ -11,4 +13,17 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all().order_by('name')
     serializer_class = ProductSerializer
-    permission_classes = [permissions.AllowAny]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        if self.action == 'retrieve':
+            return ProductDetailSerializer
+        return ProductWriteSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [IsAdminUser]
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAuthenticated]
+
+        return super(ProductViewSet, self).get_permissions()
