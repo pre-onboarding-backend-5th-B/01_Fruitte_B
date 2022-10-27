2cfedcbb69810a15f28c7d70540983ee4c9cfb1a
jslee <dlwhdtjd012@kakao.com>
2022-10-27 23:29:09 +0900

    feat(product.views): ProductOptionViewSet 생성

--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from product.models import Product
+from product.models import Product, ProductOption
 from product.serializers import ProductSerializer, ProductReadOnlySerializer, ProductDetailOrWriteSerializer, \
     ProductOptionSerializer
 
@@ -26,3 +27,27 @@ class ProductViewSet(viewsets.ModelViewSet):
             self.permission_classes = [IsAuthenticated]
 
         return super(ProductViewSet, self).get_permissions()
+
+
+class ProductOptionViewSet(viewsets.ModelViewSet):
+    """
+    상품 가격을 등록, 수정, 조회, 삭제를 할 수 있음
+    이때, 권한에 따라 조회만 할 수도 있고, CRUD 를 할 수 있음.
+    """
+    queryset = ProductOption.objects.all().order_by('product_id')
+    serializer_class = ProductOptionSerializer
+
+    def perform_create(self, serializer):
+        product = get_object_or_404(Product, pk=self.kwargs['product_id'])
+        serializer.save(product=product)
+        return super().perform_create(serializer)
+
+    def get_queryset(self):
+        return ProductOption.objects.filter(product_id=self.kwargs['product_id'])
+
+    def get_permissions(self):
+        self.permission_classes = [IsAdminUser]
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAuthenticated]
+
+        return super(ProductOptionViewSet, self).get_permissions()
