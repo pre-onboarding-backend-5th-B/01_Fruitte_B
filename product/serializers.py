93ef896c7ff14166e57fbf31d299152ca03b1f38
jslee <dlwhdtjd012@kakao.com>
2022-10-27 22:26:01 +0900

    feat(product.serializers): ProductOptionSerializer 추가.

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, ProductOption
+
+
+class ProductOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOption
+        read_only_fields = ('id', 'product',)
+        fields = ['option_detail', 'price', 'amount']
 
 
 class ProductSerializer(serializers.ModelSerializer):
