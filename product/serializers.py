eb6cbc05d43c35fb65ac5e326bd9908c0e547542
jslee <dlwhdtjd012@kakao.com>
2022-10-27 12:02:14 +0900

    feat(product.serializer): List, Detail, Write Serializer create

--- product/serializers.py
+++ product/serializers.py
@@ -7,3 +7,18 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class ProductListSerializer(ProductSerializer):
+    class Meta(ProductSerializer.Meta):
+        fields = ['name', 'description', 'thumbnail']
+
+
+class ProductDetailSerializer(ProductSerializer):
+    class Meta(ProductSerializer.Meta):
+        fields = ['name', 'description', 'thumbnail', 'detail_image', 'description']
+
+
+class ProductWriteSerializer(serializers.ModelSerializer):
+    class Meta(ProductSerializer.Meta):
+        fields = ['name', 'description', 'thumbnail', 'detail_image']
