5cc8d8ca4f9878f2ae421e3f5cd3717e2d80862a
Jong012 <Jong012>
2022-10-31 22:05:02 +0900

    feat(purchase.views): PayMethodViewSet, DeliveryStatusViewSet get_permissions 설정

--- purchase/views.py
+++ purchase/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from cart.models import CartItem, Cart
+from product.models import ProductOption
 from purchase.models import PurchasedList, PayMethod, DeliveryStatus
 from purchase.serializers import PurchasedListSerializer, PayMethodSerializer, DeliveryStatusSerializer
 
@@ -10,10 +14,24 @@ class PayMethodViewSet(ModelViewSet):
     serializer_class = PayMethodSerializer
     queryset = PayMethod.objects.all()
 
+    def get_permissions(self):
+        self.permission_classes = [IsAdminUser]
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAuthenticated]
+
+        return super(PayMethodViewSet, self).get_permissions()
+
 
 class DeliveryStatusViewSet(ModelViewSet):
     serializer_class = DeliveryStatusSerializer
     queryset = DeliveryStatus.objects.all()
+
+    def get_permissions(self):
+        self.permission_classes = [IsAdminUser]
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAuthenticated]
+
+        return super(DeliveryStatusViewSet, self).get_permissions()
 
 
 class PurchaseViewSet(ModelViewSet):
