89b34d63b4a4d583f6134e544dc05bf929bc051d
Jong012 <Jong012>
2022-10-31 22:06:31 +0900

    feat(purchase.views): PurchaseViewSet permission 설정, queryset, transaction 설정
    1. permission 로그인한 사용자만 접근하도록 설정
    2. queryset 에서 본인 것만 보도록 설정
    3. 재고 수량을 위한 transaction 설정

--- purchase/views.py
+++ purchase/views.py
@@ -37,3 +37,34 @@ class DeliveryStatusViewSet(ModelViewSet):
 class PurchaseViewSet(ModelViewSet):
     serializer_class = PurchasedListSerializer
     queryset = PurchasedList.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # 본인 것만 봐야함
+        return PurchasedList.objects.select_related('cart').filter(
+            cart__user=self.request.user
+        )
+
+    # 구매 내역의 create 는 결제 하기 기능과 같다고 생각
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            cart = Cart.objects.get(pk=serializer.validated_data.get('cart').id)
+            if not self.request.user == cart.user:
+                raise ValidationError('결제는 장바구니를 담은 사용자와 일치해야 합니다.')
+            cart_items = CartItem.objects.select_related('option').filter(
+                cart=cart
+            ).order_by('product_id')  # cart_item 에 담긴 부분을 확인함
+            product_details = ProductOption.objects.filter(
+                id__in=[x.option.id for x in cart_items]  # cart_item 에 담긴 product detail 을 확인, 재고 수량을 줄임
+            ).order_by('product_id')
+
+            assert len(cart_items) == len(product_details)
+            for cart_item, product_detail in zip(cart_items, product_details):
+                product_detail.amount -= cart_item.quantity
+                if product_detail.amount < 0:
+                    raise ValidationError(f'재고가 부족합니다. '
+                                          f'현재 {product_detail.product.name}은/는 {product_detail.amount}개 남았습니다.')
+                product_detail.save()
+
+            purchase = serializer.save(cart=cart, user=self.request.user)
+            return purchase
