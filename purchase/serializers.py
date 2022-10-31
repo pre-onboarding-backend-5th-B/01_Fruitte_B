82dd6fe15193766801e2dc195a9841e20d4a3586
Jong012 <Jong012>
2022-10-31 18:12:42 +0900

    feat(purchase.serializer): PurchasedListSerializer fields 수정

--- purchase/serializers.py
+++ purchase/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from cart.serializers import CartItemSerializer
 from purchase.models import PayMethod, DeliveryStatus, PurchasedList
 
 
@@ -30,7 +31,13 @@ class PurchasedListSerializer(serializers.ModelSerializer):
     """
     결제 수단
     """
+    cart_items_detail = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_cart_items_detail(obj):
+        product_detail = obj.cart.cartitem_set.all()
+        return CartItemSerializer(product_detail, many=True).data
 
     class Meta:
         model = PurchasedList
-        fields = '__all__'
+        fields = ['cart', 'cart_items_detail', 'pay_method', 'delivery_status', 'is_present']
